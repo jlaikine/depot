2e4307f3d9b2da80d38fc9523ffaab7492da462d
Guilhem Cambon <gcambo01@pcalb-mm0000.univ-jfc.fr>
2021-02-10 12:00:55 +0100

    ajout fermeture fenetre

--- src/projet/Application.java
+++ src/projet/Application.java
@@ -1,8 +1,10 @@
 package projet;
 
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Application extends Frame {
+public class Application extends Frame implements WindowListener {
 
 	public static void main(String[] args) {
 		new Application();
@@ -17,9 +19,51 @@ public class Application extends Frame {
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
 		
+		this.addWindowListener(this);
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0); 
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
